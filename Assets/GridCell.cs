f4f8143b0956dedb24fa9b1c12c66499ec55c842
Taylor <knexer@gmail.com>
2016-12-10 21:56:49 +0100

    Shitty grid thing

---
+++ Assets/GridCell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell : MonoBehaviour {
+    public MachineGrid Grid;
+    public int X;
+    public int Y;
+
+    public GameObject CellMachine;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/MachineGrid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineGrid : MonoBehaviour {
+
+    public int Width;
+    public int Height;
+    public GameObject GridCellPrefab;
+
+    public GameObject[,] GridCells;
+    public GameObject[,] GridVertices;
+
+    private void Awake() {
+        GridCells = new GameObject[Width, Height];
+        GridVertices = new GameObject[Width + 1, Height + 1];
+    }
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
