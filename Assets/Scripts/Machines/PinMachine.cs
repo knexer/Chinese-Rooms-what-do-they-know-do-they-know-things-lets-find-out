7f9a0d705316ef446256db94a2309e938f350f86
Taylor <knexer@gmail.com>
2016-12-11 13:02:44 -0800

    Pins in front of a tablet stop movement

---
+++ Assets/Scripts/Machines/PinMachine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinMachine : VertexMachine {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    protected override void Manipulate(float tickTime)
+    {
+        // no-op
+    }
+}
