a8a2be646c16425fa1e2d98ee2ee5fa485c980b0
Emma Breen <emmabreen@Emmas-MacBook-Pro.local>
2016-12-11 15:39:44 -0800

    Removed conditional mover.  Sprite should be added to mover instead.

--- Assets/Scripts/Machines/ConditionalCellMachine.cs
+++ Assets/Scripts/Machines/ConditionalCellMachine.cs
@@ -3,5 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class ConditionalCellMachine : CellMachine {
+	//public override void OnPlace() {
+		// GridCell.GetSurroundingVertices();
+		// Get Conditional and normal movers
+		// Change all normal movers to conditional.
+		// For all conditional, add new condition.
 
+		// Change all movers to conditional movers.
+	//}
+
+	//public override void OnRemove() {
+		// GridCell.GetSurroundingVertices();
+		// Get Conditional movers
+		// For all conditional movers, remove condition.
+	//}
 }
