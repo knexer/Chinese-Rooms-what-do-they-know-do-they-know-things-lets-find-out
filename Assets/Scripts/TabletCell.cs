033a2d2ba43b712d9009476b53a1deb775188709
Max Payton <maxspayton@gmail.com>
2016-12-11 13:50:33 -0800

    Got the stampers working

--- Assets/Scripts/TabletCell.cs
+++ Assets/Scripts/TabletCell.cs
@@ -2,47 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TabletCell : MonoBehaviour {
-  public tk2dSpriteAnimator tabletAnimator;
+public class TabletCell : MonoBehaviour
+{
+    public tk2dSpriteAnimator tabletAnimator;
 
-	public enum Colors {
-		None,
-		Green,
-		Blue
-	}
-
-	public enum Symbols {
-		Eye,
-		Snake,
-		Dream
-	}
-
-	public Colors Color;
-	public Symbols Symbol;
-
-	// Use this for initialization
-	void Start () {
-    SetSymbol(Symbol);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-  private void SetSymbol (Symbols symbol) {
-    switch (symbol) {
-    case Symbols.Eye:
-      tabletAnimator.SetFrame(0);
-      break;
-
-    case Symbols.Snake:
-      tabletAnimator.SetFrame(1);
-      break;
-
-    case Symbols.Dream:
-      tabletAnimator.SetFrame(2);
-      break;
+    public enum Colors
+    {
+        None,
+        Green,
+        Blue
     }
-  }
+
+    public enum Symbols
+    {
+        Eye,
+        Snake,
+        Dream
+    }
+
+    private Color color;
+    private Symbols symbol;
+
+    public Colors Color;
+    public Symbols Symbol
+    {
+        get { return symbol; }
+        set { SetSymbol(value); symbol = value; }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SetSymbol(Symbol);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetSymbol(Symbols symbol)
+    {
+        switch (symbol)
+        {
+            case Symbols.Eye:
+                tabletAnimator.SetFrame(0);
+                break;
+
+            case Symbols.Snake:
+                tabletAnimator.SetFrame(1);
+                break;
+
+            case Symbols.Dream:
+                tabletAnimator.SetFrame(2);
+                break;
+        }
+    }
 }
