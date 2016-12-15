a78959f63ed6958321e677d7857baf91c6d03cc7
knexer <knexer@gmail.com>
2016-12-14 23:55:33 -0800

    Cycles detected

--- Assets/Scripts/Tablet/RawTablet.cs
+++ Assets/Scripts/Tablet/RawTablet.cs
@@ -15,9 +15,38 @@ public class RawTablet : ITablet {
         BottomLeft = new RawTabletCell();
         BottomRight = new RawTabletCell();
     }
+
+    public override bool Equals(object obj)
+    {
+        ITablet otherTablet = obj as ITablet;
+        if (otherTablet == null) return false;
+
+        return this.TabletEquals(otherTablet);
+    }
+
+    public override int GetHashCode()
+    {
+        return TopLeft.GetHashCode()
+            + TopRight.GetHashCode() * 17
+            + BottomLeft.GetHashCode() * 17 * 17
+            + BottomRight.GetHashCode() * 17 * 17 * 17;
+    }
 }
 
 public class RawTabletCell : ITabletCell {
     public TabletColor Color { get; set; }
     public TabletSymbol Symbol { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        ITabletCell otherTablet = obj as ITabletCell;
+        if (otherTablet == null) return false;
+
+        return this.TabletEquals(otherTablet);
+    }
+
+    public override int GetHashCode()
+    {
+        return Color.GetHashCode() + 17 * Symbol.GetHashCode();
+    }
 }
